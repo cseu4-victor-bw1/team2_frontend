1cc5941051ee680ac689434b3efe643811bf124b
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-01 08:58:01 +0100

    style(main): 💄 better looking navbar

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,28 +2,9 @@ import React, { useState } from "react";
 import axiosWithAuth from "../helpers/axios";
 
 export default function MainPage(props) {
-	// const [jokeofDay, setjokeofDay] = useState({});
-
-	const getJokeofday = () => {};
-
-	React.useEffect(() => {
-		axiosWithAuth()
-			.get("public/jokes/thehour")
-			.then((response) => {
-				// setjokeofDay(response.data);
-				// props.values = response.data;
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
-
 	return (
 		<div className="mainpage">
-			<h1 className="hourJoke">Main page?</h1>
-			{/* <Row>
-				<JokesList {...props} api="public/jokes/popular" />
-			</Row> */}
+			<h1 style={{ color: "white" }}>Main page</h1>
 		</div>
 	);
 }
